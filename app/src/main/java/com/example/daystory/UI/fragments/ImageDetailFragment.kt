6cad298dc8f7f7cc0cf36919ac84828ac59ee1f8
yusufksimsek <yusuf.simsek2015@outlook.com>
2024-06-10 22:19:25 +0300

    Date format added into textView in ImageDetailFragment

--- app/src/main/java/com/example/daystory/UI/fragments/ImageDetailFragment.kt
+++ app/src/main/java/com/example/daystory/UI/fragments/ImageDetailFragment.kt
@@ -5,10 +5,14 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
 import androidx.navigation.fragment.navArgs
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.daystory.UI.adapter.ImageDetailAdapter
 import com.example.daystory.databinding.FragmentImageDetailBinding
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
 
 class ImageDetailFragment : Fragment() {
     private lateinit var binding: FragmentImageDetailBinding
@@ -24,7 +28,12 @@ class ImageDetailFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        binding.iconBack.setOnClickListener {
+            it.findNavController().popBackStack()
+        }
+
         setupRecyclerView()
+        setDate()
         val events = args.events!!.toList()
         imageDetailAdapter.submitList(events)
     }
@@ -35,5 +44,9 @@ class ImageDetailFragment : Fragment() {
             layoutManager = LinearLayoutManager(context)
             adapter = imageDetailAdapter
         }
+    }
+    private fun setDate() {
+        val currentDate = SimpleDateFormat("dd-MMus-yyy", Locale.getDefault()).format(Date())
+        binding.textViewDate.text = currentDate
     }
 }
