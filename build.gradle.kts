c8bf76988c97adde49aed7aff731dec57577d1a2
ferityuksel <153657942+ferityuksel@users.noreply.github.com>
2024-06-11 15:11:14 +0300

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,9 @@
-buildscript{
-    repositories{
+buildscript {
+    repositories {
         google()
     }
 
-    dependencies{
+    dependencies {
         classpath("androidx.navigation:navigation-safe-args-gradle-plugin:2.7.7")
     }
 }
@@ -12,5 +12,5 @@ plugins {
     alias(libs.plugins.androidApplication) apply false
     alias(libs.plugins.jetbrainsKotlinAndroid) apply false
     id("com.google.devtools.ksp") version "1.9.0-1.0.13" apply false
-    id "org.sonarqube" version "5.0.0.4638"
+    id("org.sonarqube") version "5.0.0.4638"
 }
