d00188f8366b1b0997ffd5473f9b71ce634d671c
yusufksimsek <yusuf.simsek2015@outlook.com>
2024-06-11 17:28:59 +0300

    GalleryDetail back icon solved and GalleryDetailAdapter created

--- app/src/main/java/com/example/daystory/UI/fragments/GalleryDetailFragment.kt
+++ app/src/main/java/com/example/daystory/UI/fragments/GalleryDetailFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.findNavController
 import androidx.navigation.fragment.navArgs
 import com.example.daystory.databinding.FragmentGalleryDetailBinding
 
@@ -15,6 +16,10 @@ class GalleryDetailFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         binding = FragmentGalleryDetailBinding.inflate(inflater, container, false)
+
+        binding.iconBack.setOnClickListener {
+            it.findNavController().popBackStack()
+        }
 
         val args: GalleryDetailFragmentArgs by navArgs()
         binding.textViewDate.text = args.date
