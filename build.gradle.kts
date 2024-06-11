5594de02ba329d1e14749fd9bb171ec832b4c0b1
ferityuksel <153657942+ferityuksel@users.noreply.github.com>
2024-06-11 15:08:34 +0300

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,7 +12,5 @@ plugins {
     alias(libs.plugins.androidApplication) apply false
     alias(libs.plugins.jetbrainsKotlinAndroid) apply false
     id("com.google.devtools.ksp") version "1.9.0-1.0.13" apply false
-}
-plugins {
-  id "org.sonarqube" version "5.0.0.4638"
+    id "org.sonarqube" version "5.0.0.4638"
 }
